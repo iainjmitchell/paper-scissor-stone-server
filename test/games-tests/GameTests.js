63b030303d6c89af8b76eb56877bf05d0efb8479
iainjmitchell <iainjmitchell@gmail.com>
2014-08-08 17:00:21 +0100

    refactoring Yo!

--- test/games-tests/GameTests.js
+++ test/games-tests/GameTests.js
@@ -1,6 +1,8 @@
 require('chai').should();
 
 describe('When a game of paper, scissors, stone is started', function(){
+	var alwaysStone = alwaysReturns('stone');
+
 	describe('And a competitor is registered', function(){
 		describe('and round is started', function(){
 			it('Then competitor score on display is set to 0 (there is no one to fight)', function(done){
@@ -32,6 +34,30 @@ describe('When a game of paper, scissors, stone is started', function(){
 			});
 		});
 	});
+
+	// describe('And two competitors are registered one of which returns no moves', function(){
+	// 	describe('and round is started', function(){
+	// 		it('Then both competitors score on display is set to 0', function(){
+	// 			var noMoves = [],
+	// 				competitor1 = new MockCompetitor(alwaysStone),
+	// 				competitor2 = new MockCompetitor(noMoves);	
+	// 			new Game()
+	// 				.addCompetitor(competitor1)
+	// 				.addCompetitor(competitor2)
+	// 				.startRound();
+	// 			competitor1.score.should.equal(1);
+	// 			competitor2.score.should.equal(0);
+	// 		});
+	// 	});
+	// });
+
+	function alwaysReturns(move){
+		var count = 200,
+			moves = [];
+		for(count; count < 200; count--){
+			moves.push(move);
+		}
+	}
 
 	var MockCompetitor = function(moves){
 		var moves = 0;
