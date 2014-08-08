1c5e8954f6bce771096ee0cb2a59edf1cbef558b
iainjmitchell <iainjmitchell@gmail.com>
2014-08-08 09:05:14 +0100

    show added competitor

--- test/games-tests/GameTests.js
+++ test/games-tests/GameTests.js
@@ -1,9 +1,22 @@
 require('chai').should();
 
 describe('When a game of paper, scissors, stone is started', function(){
-	describe('And no competitors have been registered', function(){
-		it('Then no ', function(){
-
+	describe('And a competitor is registered', function(){
+		it('Then the display is updated with the competitor', function(done){
+			var newCompetitor = {},
+				mockDisplay = {
+					showCompetitor : function(competitor){
+						competitor.should.equal(newCompetitor);
+						done();
+					}
+				};	
+			new Game(mockDisplay).addCompetitor(newCompetitor);
 		});
 	});
 });
+
+var Game = function(display){
+	this.addCompetitor = function(competitor){
+		display.showCompetitor(competitor);
+	};
+};
