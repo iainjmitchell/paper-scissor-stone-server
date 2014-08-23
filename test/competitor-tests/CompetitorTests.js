60c9d4e72dd4e756f06f3fd57a6beef345acaf71
iainjmitchell <iainjmitchell@gmail.com>
2014-08-23 13:09:16 +0100

    notifying creation of competitor

---
+++ test/competitor-tests/CompetitorTests.js
@@ -0,0 +1,23 @@
+require('chai').should();
+
+var FakeEventStore = function(){
+	this.events = {};
+	
+	this.notify = function(eventName, event){
+		this.events[eventName] = event;
+	};
+};
+
+
+describe('When a competitor is created', function(){
+	it('Then a new competitor event is raised with name of competitor', function(){
+		var competitorName = 'a Name ' + Math.random(),
+			fakeEventStore = new FakeEventStore();
+		new Competitor(fakeEventStore, {name : competitorName});
+		fakeEventStore.events['newCompetitor'].name.should.equal(competitorName);
+	});
+});
+
+var Competitor = function(eventStore, competitorDetails){
+	eventStore.notify('newCompetitor', { name : competitorDetails.name});
+};
