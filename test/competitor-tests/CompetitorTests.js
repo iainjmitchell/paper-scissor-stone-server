54b2adacbc9e2e3ea5424f960f0223b154faef1d
iainjmitchell <iainjmitchell@gmail.com>
2014-08-23 18:06:59 +0100

    event raised when second new round started

--- test/competitor-tests/CompetitorTests.js
+++ test/competitor-tests/CompetitorTests.js
@@ -26,18 +26,32 @@ describe('When a competitor is created', function(){
 				.roundStarted();
 			fakeEventStore.events['newRoundStarted'].number.should.equal(1);
 		});
+
+		describe('When another round is started', function(){
+			it('Then a new round started event is raised with round number 2', function(){
+				var fakeEventStore = new FakeEventStore();
+				new Competitor(fakeEventStore, {})
+					.roundStarted()
+					.roundStarted();
+				fakeEventStore.events['newRoundStarted'].number.should.equal(2);
+			});
+		});
 	});
 });
 
 var Competitor = function(eventStore, competitorDetails){
-	var NEW_COMPETITOR_EVENT = 'newCompetitor';
+	var NEW_COMPETITOR_EVENT = 'newCompetitor',
+		NEW_ROUND_EVENT = 'newRoundStarted',
+		numberOfRounds = 0;
 
 	function init(){
 		eventStore.notify(NEW_COMPETITOR_EVENT, { name : competitorDetails.name});
 	}
 
 	this.roundStarted = function(){
-		eventStore.notify('newRoundStarted', {number : 1});
+		numberOfRounds++;
+		eventStore.notify(NEW_ROUND_EVENT, {number : numberOfRounds});
+		return this;
 	};
 
 	init();
