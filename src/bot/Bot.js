877f9a542793bbf88972b005900220ab8a38e240
iainjmitchell <iainjmitchell@gmail.com>
2014-09-02 14:18:57 +0100

    bot changes

--- src/bot/Bot.js
+++ src/bot/Bot.js
@@ -1,7 +1,4 @@
 var httpsync = require('httpsync');
-// var req = httpsync.get({ url : "http://localhost:1337"});
-// var res = req.end();
-// console.log(res.data.toString());
 
 var Bot = function(uri){
 	var START_ROUND_REQUEST = { url : uri + '/round', method : 'POST' },
