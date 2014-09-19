45a373ec813f9cba8748ab30420d61a0d4ff8284
iainjmitchell <iainjmitchell@gmail.com>
2014-09-19 16:53:11 +0100

    removing consoel logs

--- public/javascripts/EventStore.js
+++ public/javascripts/EventStore.js
@@ -6,11 +6,7 @@
 			console.log('event store notified');
 			var count = 0,
 				callbacks = events[eventName];
-			console.log(events);
-			console.log(eventName);
 			if (!!callbacks){
-				console.log('callbacks');
-				console.log(callbacks);
 				for(count; count < callbacks.length; callbacks++){
 					console.log('asdasdad');
 					callbacks[0](event);
