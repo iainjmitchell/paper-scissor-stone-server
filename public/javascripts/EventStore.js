d91ed2b6198377a854601f51d6af0fb8ccdc2e84
iainjmitchell <iainjmitchell@gmail.com>
2014-09-19 16:52:17 +0100

    ensuring that correct event is raised from socket io

--- public/javascripts/EventStore.js
+++ public/javascripts/EventStore.js
@@ -3,10 +3,16 @@
 		var events = {};
 
 		this.notify = function(eventName, event){
+			console.log('event store notified');
 			var count = 0,
 				callbacks = events[eventName];
+			console.log(events);
+			console.log(eventName);
 			if (!!callbacks){
+				console.log('callbacks');
+				console.log(callbacks);
 				for(count; count < callbacks.length; callbacks++){
+					console.log('asdasdad');
 					callbacks[0](event);
 				}
 			}
--- public/javascripts/IOEventAdapter.js
+++ public/javascripts/IOEventAdapter.js
@@ -1,18 +1,24 @@
 (function(module, io, undefined){
 	module.IOEventAdapter = function(events, eventStore){
-		var socket = io(document.URL);
-		console.log(document.URL);
+		var socket = io(document.URL),
+			eventHandlerFactory = new EventHandlerFactory(eventStore);
 
 		function init(){
 			var count = 0;
 			for(count; count < events.length; count++){
 				var eventName = events[count];
-				socket.on(eventName, function (data){
-			        eventStore.notify(eventName, data);
-			    });
+				socket.on(eventName, eventHandlerFactory.create(eventName));
 			}
 		};
 
 		init();
+	};
+
+	var EventHandlerFactory = function(eventStore){
+		this.create = function(eventName){
+			return function(data){
+				eventStore.notify(eventName, data);
+			};
+		};
 	};
 })(window, io);
